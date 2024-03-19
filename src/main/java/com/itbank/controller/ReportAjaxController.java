d10d1cce77d27b77fee385936ba95203d003e824
javagangyong <kenjisun@naver.com>
2024-03-19 09:52:12 +0900

    갑니다 경용

--- src/main/java/com/itbank/controller/ReportAjaxController.java
+++ src/main/java/com/itbank/controller/ReportAjaxController.java
@@ -1,10 +1,14 @@
 package com.itbank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.itbank.model.ReportDTO;
 import com.itbank.service.ReportService;
@@ -16,10 +20,39 @@ public class ReportAjaxController {
 	@Autowired private ReportService rs;
 
 	@PostMapping("report")
-	public int addReport(@RequestBody ReportDTO dto) {
+	public int addReport(ReportDTO dto) {
 		int row = rs.add(dto);
 		System.out.println(row != 0 ? "신고 완료" : "신고 실패");
 		return row;
 	}
+	
+	@GetMapping("list")
+	public List<ReportDTO> reportList(@RequestParam String userid) {
+		System.out.println(userid + " ajaxController");
+		List<ReportDTO> list = rs.getReportList(userid);
+		System.out.println(list);
+		return list;
+	}
+	
+	@GetMapping("deleteReport")
+	public int deleteReport(@RequestParam int idx) {
+		int row = rs.deleteReport(idx);;
+		return row;
+	}
+	
+	@GetMapping("view")
+	public ReportDTO viewModify(@RequestParam int idx) {
+		ReportDTO dto = rs.getView(idx);
+		System.out.println("viewdto = " + dto);
+		return dto;
+	}
+	
+	@PostMapping("reportModify")
+	public int reportModify(ReportDTO dto) {
+		int row = rs.updateModify(dto);
+		System.out.println("row = " + row);
+		return row;
+	}
+	
 	
 }
